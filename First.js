07e2c4a811d769e6a6839ae4e3381bcddf9e6f45
=Premkumar Savale <premsavle112@gmail.com>
2025-12-29 22:40:47 +0530

    Added Some New Topic

--- First.js
+++ First.js
@@ -1,2 +1,7 @@
-document.write("Vaibhav Savale");
- 
+
+document.write("Hello , Welcome to Java Script Languge....")
+
+ document.write("<br>")
+
+ document.write("This is Java Script Language ");
+  
