461ee0b12c6a86cf057b2876f391c09b749b2f11
Premkumar Savale <premsavle112@gmail.com>
2026-01-01 17:11:46 +0530

    Added DATA

---
+++ Pratical_2.js
@@ -0,0 +1,54 @@
+// datatype ...
+
+
+
+
+var a = "Premkumar Savale "
+
+console.log(a);
+
+var b = 12;
+
+console.log(b);
+
+var c = true;
+
+console.log(c);
+
+
+var d = null;
+
+console.log(d);
+
+
+var e;
+
+console.log(e);
+
+
+
+
+let user = {
+
+    name: 'Premkumar',
+    age: 20,
+};
+
+console.log(user.name);
+
+
+
+const data = ["Vaibha", "Ajay", "Atul", "Amol"];
+
+console.log(data)
+
+
+
+const Ab = () => { 
+
+     console.log("Ab");
+      
+}
+
+Ab();
+ 
